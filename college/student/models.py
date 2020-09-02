90e05c6e02c8f06eae61bafa53fae479a9b294d6
sudha <sudha.bhatta@launchship.com>
2020-09-02 15:55:29 +0530

    student model

--- college/student/models.py
+++ college/student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	First_name = models.CharField(max_length= 20)
+	Last_name = models.CharField(max_length=20)
+	Branch = models.CharField(max_length=10)
+	Age = models.IntegerField()
+	Email = models.EmailField()
